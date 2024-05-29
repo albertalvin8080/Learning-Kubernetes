90e8e0caf526a800373451b3a80d871b81c17506
AlbertAlvi <albertalvin99@gmail.com>
2024-05-29 20:01:44 -0300

    netpol

--- 21-networkPolicy/.apply.bat
+++ 21-networkPolicy/.apply.bat
@@ -1,1 +1,7 @@
-kubectl apply -f ns-backend.yaml -f ns-frontend.yaml -f nginx.yaml -f busybox.yaml
+kubectl apply ^
+    -f ns-backend.yaml ^
+    -f ns-frontend.yaml ^
+    -f ns-production.yaml ^
+    -f nginx.yaml ^
+    -f busybox.yaml ^
+    -f spring.yaml 
